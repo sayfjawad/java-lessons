0db3c3fd350dfad43ecfbc1fdffa9404da0e3ab7
sayf <sayf@multicode.nl>
2024-01-29 17:49:12 +0100

    Lesson 4

---
+++ Lesson 4/src/VariableLesson.java
@@ -0,0 +1,11 @@
+public class VariableLesson {
+
+    public static void main(String[] args) {
+
+        String firstName = "Sayf";
+        String lastName = "Jawad";
+        int age = 44;
+
+        System.out.println("Mr. " + firstName + " " + lastName + " has reached the age of " + age);
+    }
+}
