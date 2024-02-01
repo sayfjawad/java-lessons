943e679d7a6b42ffb7fa1b297e0f089976db7d75
sayf <sayf@multicode.nl>
2024-02-01 18:06:02 +0100

    Lesson 5

--- Lesson 5/src/Starter.java
+++ Lesson 5/src/Starter.java
@@ -2,6 +2,12 @@
 public class Starter {
 
     public static void main(String[] args) {
+
+        float third = 100_000.111f;
+        short second = (short)third;
+
+        System.out.println("We've got: " + second);
+
     }
 }
 
