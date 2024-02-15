ad9f2b4a80e68f1118abf7f2ab47ad73955f73b4
sayf <sayf@multicode.nl>
2024-02-15 21:55:23 +0100

    lesson 7

---
+++ src/main/java/lesson7/MyArraysApp.java
@@ -0,0 +1,17 @@
+package lesson7;
+
+public class MyArraysApp {
+
+    public static void main(String[] args){
+
+        String[] myWordsArray = new String[4];
+
+        myWordsArray[0] = "Welcome";
+        myWordsArray[1] = "to";
+        myWordsArray[2] = "learning";
+        myWordsArray[3] = "Java";
+
+
+        System.out.println(myWordsArray[1]);
+    }
+}
